294664954c2d3e2a0932ad87d164b41d4883fddf
Kevin Fox <Kevin.Fox@pnnl.gov>
2017-01-25 00:01:29 +0000

    Bump to 0.5.0

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,17 @@ MAINTAINER Kevin Fox "Kevin.Fox@pnnl.gov"
 USER root
 RUN apt-get update -y; apt-get install -y postgresql-client
 ADD ./start.sh /etc/start.sh
-ADD ./b2s_0.4.0.tar.gz /opt
+ADD ./b2s_0.5.0.tar.gz /opt
 #FIXME pull binaries from somewhere else.
 #FIXME add passwordfile thing to the create_domain script native.
 RUN mkdir -p /opt && \
 cd /opt && \
 chown --recursive payara.payara /opt/b2s && \
 sed -i "s@\(/opt/payara41\)_b2s@\1@g" /opt/b2s/b2s_env && \
+sed  -i "s@B2S_INITIAL_MEM=-Xms8192m@B2S_INITIAL_MEM=-Xms2048m@" /opt/b2s/b2s_env && \
+sed  -i "s@B2S_MAX_MEM=-Xmx8192m@B2S_MAX_MEM=-Xmx4096m@" /opt/b2s/b2s_env && \
 chmod +x /etc/start.sh && \
-rm -f /b2s_0.4.0.tar.gz && \
+rm -f /b2s_0.5.0.tar.gz && \
 mkdir -p /srv/belle2_conddb && \
 chown --recursive payara.payara /srv
 
@@ -20,6 +22,7 @@ USER payara
 RUN cd /opt/b2s && \
 bash -xe create_domain.sh && \
 . ./b2s_env && \
-asadmin --user admin --passwordfile $GFPASSFILE --port $B2S_ADMIN_PORT stop-domain b2s
+asadmin --user admin --passwordfile $GFPASSFILE --port $B2S_DAS_ADMIN_PORT stop-domain b2s && \
+asadmin --user admin --passwordfile $GFPASSFILE --port $B2S_DAS_ADMIN_PORT stop-local-instance b2sServer
 
 CMD ["/etc/start.sh"]
--- start.sh
+++ start.sh
@@ -14,18 +14,23 @@ cd /opt/b2s
 
 #FIXME set admin passwords.
 
-asadmin --passwordfile $GFPASSFILE --user admin --port 8048 start-domain b2s
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.ServerName=$PGSERVER
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.PortNumber=$PGPORT
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.Password=$(awk -F: '{if($4=="user_cond"){print $5}}' /opt/b2s/.pgpass) 2>/dev/null
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.ServerName=$PGSERVER
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.PortNumber=$PGPORT
-asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.Password=$(awk -F: '{if($4=="user_cond"){print $5}}' /opt/b2s/.pgpass) 2>/dev/null
-asadmin --passwordfile $GFPASSFILE --user admin --port 8048 restart-domain b2s
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT start-domain b2s
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT start-local-instance b2sServer
 
-asadmin --user admin --passwordfile $GFPASSFILE --port $B2S_ADMIN_PORT deploy ./b2s.war
 
-tail -f /opt/payara41/glassfish/domains/b2s/logs/server.log &
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.ServerName=$PGSERVER
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.PortNumber=$PGPORT
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2s_cp.property.Password=$(awk -F: '{if($4=="user_cond"){print $5}}' /opt/b2s/.pgpass) 2>/dev/null
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.ServerName=$PGSERVER
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.PortNumber=$PGPORT
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT set resources.jdbc-connection-pool.b2sAdm_cp.property.Password=$(awk -F: '{if($4=="user_cond"){print $5}}' /opt/b2s/.pgpass) 2>/dev/null
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT restart-domain b2s
+asadmin --passwordfile $GFPASSFILE --user admin --port $B2S_DAS_ADMIN_PORT restart-local-instance b2sServer
+
+asadmin --user admin --passwordfile $GFPASSFILE --port $B2S_DAS_ADMIN_PORT deploy --target b2sServer ./b2s.war
+
+#tail -f /opt/payara41/glassfish/domains/b2s/logs/server.log &
+tail -f /opt/payara41/glassfish/nodes/localhost-b2s/b2sServer/logs/server.log &
 
 #FIXME it would be nice to have a better way then this.
 while true; do
